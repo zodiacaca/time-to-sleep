8375c73444d8d2250bab4b372e35101fb2cf90d2
zodiacaca <121059464@qq.com>
2019-11-03 08:39:38 +0800

    Update ping.js

--- methods/ping.js
+++ methods/ping.js
@@ -1,7 +1,6 @@
 
 const ping = (host) => {
   const exec = require('child_process').exec
-  console.log(`@${host}`)
 
   return new Promise(resolve => {
     exec(`ping ${host}`, (error, stdout, stderr) => resolve(stdout))
